d553bc867e5569c375d8fd4f049efe2fc4f48ce4
Atheer Auribi <atheer.auribi@gmail.com>
2022-12-06 20:37:02 -0600

    added login button

--- src/Login.js
+++ src/Login.js
@@ -6,8 +6,13 @@ const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=ae075abe5e0c4
 
 export default function Login() {
     return (
-        <div>
-
-        </div>
+        <Container
+        className="d-flex justify-content-center align-items-center"
+        style={{ minHeight: "100vh" }}
+        >
+            <a className="btn btn-success btn-lg" href={AUTH_URL}>
+                Login With Spotify
+            </a>
+        </Container>
     )
 }
